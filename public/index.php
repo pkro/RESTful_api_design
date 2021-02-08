7157420b895bdaa8da5b32a04cb6b4342e55b6bc
pkro <pkrombach@web.de>
2021-02-08 12:47:54 +0100

    bootstrapping and all messages route added

--- public/index.php
+++ public/index.php
@@ -1,22 +1,29 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+require('../src/bootstrap.php');
+
+use Chatter\Models\Message;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
-
 $app = AppFactory::create();
 $app->setBasePath('/slimcourse');
 
-$app->get('/', function (Request $request, Response $response, $args) {
-  $response->getBody()->write("Hello world!");
+$app->get('/messages', function (Request $request, Response $response, $args) {
+  $_message = new Message();
+  $messages = $_message->all();
+  $payload = [];
+  foreach ($messages as $_msg) {
+    $payload[$_msg->id] = ['body' => $_msg->body, 'user_id' => $_msg->user_id, 'created_at' => $_msg->created_at];
+  }
+  $payload = json_encode($payload);
+  $response = $response->withStatus(200);
+  $response = $response->withHeader('Content-Type', 'application/json');
+  $response->getBody()->write($payload);
   return $response;
 });
-$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
-  ['name' => $name] = $args;
-  $response->getBody()->write("Hey {$name}");
-  return $response;
-});
+
 
 $app->run();
